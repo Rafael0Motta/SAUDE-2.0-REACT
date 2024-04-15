08293b42a2f527ee19ec1ca1c3f9c97859ff28d8
Rafael Motta <149005855+Rafael0Motta@users.noreply.github.com>
2024-04-15 10:39:19 -0300

    Add files via upload

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -8,6 +8,7 @@ import CheckIcon from "@mui/icons-material/Check";
 
 const Contact = () => {
   const [isCaptchaVerified, setIsCaptchaVerified] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleCaptchaVerify = () => {
     setIsCaptchaVerified(true);
@@ -35,6 +36,7 @@ const Contact = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsSubmitting(true);
     console.log("Formulário em processo de envio:", formData);
 
     try {
@@ -59,6 +61,8 @@ const Contact = () => {
     } catch (error) {
       document.getElementById("alertForm").style.display = "block";
       setAlertVisible(false);
+    }finally {
+      setIsSubmitting(false);
     }
   };
 
@@ -122,13 +126,13 @@ const Contact = () => {
             onChange={handleCaptchaVerify}
             onExpired={handleCaptchaExpire}
           />
-          <Button
+             <Button
             variant="primary"
             type="submit"
             id="form-submit"
-            disabled={!isCaptchaVerified}
+            disabled={!isCaptchaVerified || isSubmitting}
           >
-            Enviar
+            {isSubmitting ? "Enviando..." : "Enviar"}
           </Button>
         </Form>
         <div id="alertForm">
